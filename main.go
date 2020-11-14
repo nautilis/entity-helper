2f8934a007c08689cef1375203aa559bf7f9e37b
nautilis <nautilis@163.com>
2020-11-14 10:47:43 +0800

    util

--- main.go
+++ main.go
@@ -17,7 +17,6 @@ import (
 	"log"
 	"os"
 	"os/user"
-	"strings"
 	"time"
 )
 
@@ -215,40 +214,4 @@ func findEntityPositon(structName string) (startIdx int64, endIdx int64, filenam
 		})
 	}
 	return
-}
-
-func toCamelInitCase(s string, initCase bool) string {
-	s = strings.TrimSpace(s)
-	if s == "" {
-		return s
-	}
-
-	n := strings.Builder{}
-	n.Grow(len(s))
-	capNext := initCase
-	for i, v := range []byte(s) {
-		vIsCap := v >= 'A' && v <= 'Z'
-		vIsLow := v >= 'a' && v <= 'z'
-		if capNext {
-			if vIsLow {
-				v += 'A'
-				v -= 'a'
-			}
-		} else if i == 0 {
-			if vIsCap {
-				v += 'a'
-				v -= 'A'
-			}
-		}
-		if vIsCap || vIsLow {
-			n.WriteByte(v)
-			capNext = false
-		} else if vIsNum := v >= '0' && v <= '9'; vIsNum {
-			n.WriteByte(v)
-			capNext = true
-		} else {
-			capNext = v == '_' || v == ' ' || v == '-' || v == '.'
-		}
-	}
-	return n.String()
 }
